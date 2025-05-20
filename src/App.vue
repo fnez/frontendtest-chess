a8bbaaf904dc4e2c33dddb28465a2c5b0084f62d
Froi Nunez <froinunez@MacBookPro.attlocal.net>
2025-05-20 17:48:49 -0400

    Adding side panel, responsive styles

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,44 @@
     <div class="boardContainer">
       <Chessboard @squareClick="handleClick" />
     </div>
-    <Sidepanel />
+    <Sidepanel :clickedSquares="clickedList" />
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Chessboard from "./components/Chessboard.vue";
+import Sidepanel from "./components/Sidepanel.vue";
+
+const clickedList = ref([]);
 
 const handleClick = (square) => {
-  console.log(square);
+  clickedList.value.push(square);
 };
 </script>
+
+<style>
+.app {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.boardContainer {
+  min-width: 50%;
+}
+
+.chessboard-container {
+  flex: 1;
+  min-width: 300px;
+}
+
+@media (max-width: 768px) {
+  .boardContainer {
+    min-width: 100%;
+    display: flex;
+    align-self: center;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/components/Sidepanel.vue
@@ -0,0 +1,32 @@
+<template>
+  <aside class="sidebar">
+    <h2>Cliked Squares</h2>
+    <ol>
+      <li v-for="(square, index) in clickedSquares" :key="index">
+        {{ square }}
+      </li>
+    </ol>
+  </aside>
+</template>
+
+<script setup>
+defineProps({ clickedSquares: Array });
+</script>
+
+<style scoped>
+.sidebar {
+  width: 250px;
+  padding: 1rem;
+  border-left: 2px solid #ddd;
+  background: tomato;
+  color: white;
+}
+
+@media (max-width: 768px) {
+  .sidebar {
+    border-left: none;
+    border-top: 2px solid #ddd;
+    width: 100%;
+  }
+}
+</style>
